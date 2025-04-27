172ffccaff047f6e1d53ace7ee6266e4ae990cd5
さよならおやすみ <tobenaino@gmail.com>
2025-04-27 18:52:29 +0900

    Update pickup.js

--- test/sinistri/OEBPS/Misc/pickup.js
+++ test/sinistri/OEBPS/Misc/pickup.js
@@ -7,25 +7,27 @@
 		if (target) {
 			target.style.display = 'block';
 		}
-	  }
+	}
 
 	try {
-		const parentContent = window.parent.document.getElementById('content');
-		const parentClasses = parentContent ? parentContent.className : '';
-		const myTarget = document.getElementById('main');
-		if (myTarget && parentClasses) {
-			myTarget.classList.remove("serif");
-			myTarget.classList.remove("sansserif");
-			myTarget.classList.remove("bigger");
-			myTarget.classList.remove("big");
-			myTarget.classList.remove("mid");
-			myTarget.classList.remove("mini");
-			myTarget.classList.remove("minimum");
-			myTarget.className += ' ' + parentClasses;
+			console.log("フォントの設定……");
+			const parentContent = window.parent.document.getElementById('content');
+			const parentClasses = parentContent ? parentContent.className : '';
+			const myTarget = document.getElementById('main');
+			if (myTarget && parentClasses) {
+			console.log("クラス発見……" + parentClasses);
+				myTarget.classList.remove("serif");
+				myTarget.classList.remove("sansserif");
+				myTarget.classList.remove("bigger");
+				myTarget.classList.remove("big");
+				myTarget.classList.remove("mid");
+				myTarget.classList.remove("mini");
+				myTarget.classList.remove("minimum");
+				myTarget.className += ' ' + parentClasses;
+			}
+		} catch (e) {
+			console.error('親フレームにアクセスできません', e);
 		}
-	} catch (e) {
-		console.error('親フレームにアクセスできません', e);
-	}
 
 	}
 	window.addEventListener('DOMContentLoaded', showTargetSection);
