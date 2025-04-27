3c41e633fa006f4cf576433b7db1a008edc1970e
さよならおやすみ <tobenaino@gmail.com>
2025-04-27 18:49:39 +0900

    Update pickup.js

--- test/sinistri/OEBPS/Misc/pickup.js
+++ test/sinistri/OEBPS/Misc/pickup.js
@@ -1,24 +1,33 @@
-function showTargetSection() {
-	const hash = window.location.hash;
-	document.querySelectorAll('.pagination').forEach(el => { el.style.display = 'none';
-	});
-	if (hash) {
+	function showTargetSection() {
+		const hash = window.location.hash;
+		document.querySelectorAll('section').forEach(el => { el.style.display = 'none';
+		});
+		if (hash) {
 		const target = document.querySelector(hash);
 		if (target) {
 			target.style.display = 'block';
-			window.scrollTo(0, 0);
 		}
-	}
+	  }
+
 	try {
 		const parentContent = window.parent.document.getElementById('content');
 		const parentClasses = parentContent ? parentContent.className : '';
 		const myTarget = document.getElementById('main');
 		if (myTarget && parentClasses) {
+			myTarget.classList.remove("serif");
+			myTarget.classList.remove("sansserif");
+			myTarget.classList.remove("bigger");
+			myTarget.classList.remove("big");
+			myTarget.classList.remove("mid");
+			myTarget.classList.remove("mini");
+			myTarget.classList.remove("minimum");
 			myTarget.className += ' ' + parentClasses;
 		}
 	} catch (e) {
 		console.error('親フレームにアクセスできません', e);
-	}	
-}
-window.addEventListener('DOMContentLoaded', showTargetSection);
-window.addEventListener('hashchange', showTargetSection);
+	}
+
+	}
+	window.addEventListener('DOMContentLoaded', showTargetSection);
+	window.addEventListener('hashchange', showTargetSection);
+
